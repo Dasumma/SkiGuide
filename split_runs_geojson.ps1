942ffc70fe61f4c00f26253fde21cb81f8bb02f4
Dasumma <dasumma1@gmail.com>
2026-02-01 21:21:35 -0500

    Changed the split_runs_geojson script Splits the runs based on country, region, and ski area name.

--- split_runs_geojson.ps1
+++ split_runs_geojson.ps1
@@ -1,6 +1,9 @@
 function splitGeoJson {
     param (
         [string]$InputFile = "runs",
+        [string]$CountryFilter = "",
+        [string]$RegionFilter = "",
+        [string]$SkiAreaFilter = "",
         [string]$OutputDirectory = "GeoJsons"
     )
     $invalidChars = "[" + ([regex]::Escape([string]::Join('', [System.IO.Path]::GetInvalidFileNameChars()))) + "]"
@@ -13,28 +16,62 @@ function splitGeoJson {
     # 1. Read and convert the JSON file contents to PowerShell objects
     $jsonData = Get-Content -Raw -Path "$InputFile.geojson" | ConvertFrom-Json
 
+    
     Write-Host "Grouping JSON"
-    # 2. Group the objects by the specified property
-    $groupedData = $jsonData.features | Group-Object -Property {
-        $place = $_.properties.places[0].localized.en
-        "$($place.country)|$($place.region)"
+    # 2. Group the objects by country, region, and ski area
+    if ($jsonData.features[0].properties.type -eq "skiArea") {
+        $groupedData = $jsonData.features | Group-Object -Property {
+            $place = $_.properties.places[0].localized.en
+            $skiArea = $_.properties.name
+            "$($place.country)|$($place.region)|$($skiArea)"
+        }
+    }
+    else {
+        $groupedData = $jsonData.features | Group-Object -Property {
+            $place = $_.properties.places[0].localized.en
+            $skiArea = $_.properties.skiAreas[0].properties.name
+            "$($place.country)|$($place.region)|$($skiArea)"
+        }
     }
 
     Write-Host 'Iterating Through Groups'
     # 3. Iterate through each group and export the items to a new JSON file
     foreach ($group in $groupedData) {
+
         # Define the output file name based on the group name (property value)
         $index = $group.Name.IndexOf("|")
-        $country = $group.Name.Substring(0, $index)
-        $region = $group.Name.Substring($index + 1)
+        $indexTwo = $group.Name.Substring($group.Name.IndexOf("|") + 1).IndexOf("|") + $index
 
+        $country = $group.Name.Substring(0, $index)
+        if ($country -eq "") { $country = "null" }
+        $region = $group.Name.Substring($index + 1, $indexTwo - $index)
+        if ($region -eq "") { $region = "null" }
+        $skiArea = $group.Name.Substring($indexTwo + 1)
+        if ($skiArea -eq "") { $skiArea = "null" }
+
+        
+        if ($CountryFilter -ne "" -and -not $CountryFilter.Contains($country)) {
+            continue
+        }
+        if ($RegionFilter -ne "" -and -not $RegionFilter.Contains($region)) {
+            continue
+        }
+        if ($SkiAreaFilter -ne "" -and -not $SkiAreaFilter.Contains($skiArea)) {
+            continue
+        }
+        
         $countryFolder = Join-Path -Path $OutputDirectory -ChildPath $country 
         if (-not (Test-Path $countryFolder)) {
             New-Item -Path $countryFolder -ItemType Directory | Out-Null
         }
+        
+        $regionFolder = Join-Path -Path $countryFolder -ChildPath $region
+        if (-not (Test-Path $regionFolder)) {
+            New-Item -Path $regionFolder -ItemType Directory | Out-Null
+        }
     
-        $outputFileName = ("$($InputFile)_$($region).geojson") -replace $invalidChars, ""
-        $outputPath = Join-Path -Path $countryFolder -ChildPath $outputFileName
+        $outputFileName = ("$($InputFile)_$($skiArea).geojson") -replace $invalidChars, ""
+        $outputPath = Join-Path -Path $regionFolder -ChildPath $outputFileName
 
         $group.Group | ConvertTo-Json -Depth 100 | Set-Content -Path $outputPath
         Write-Host "Created file: $outputPath"
