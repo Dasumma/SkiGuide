b63f4f3882361a467904eed341fe84473720b3bd
Dasumma <dasumma1@gmail.com>
2026-02-07 22:50:59 -0500

    Android Init

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -46,6 +46,7 @@ dependencies {
     implementation(libs.androidx.compose.ui.graphics)
     implementation(libs.androidx.compose.ui.tooling.preview)
     implementation(libs.androidx.compose.material3)
+    implementation("com.mapbox.maps:android-ndk27:11.18.1")
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
--- app/src/main/java/com/dasumma/mobileskinav/MainActivity.kt
+++ app/src/main/java/com/dasumma/mobileskinav/MainActivity.kt
@@ -1,5 +1,7 @@
 package com.dasumma.mobileskinav
 
+import android.Manifest
+import android.annotation.SuppressLint
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
@@ -11,24 +13,66 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import com.mapbox.geojson.Point
+import com.mapbox.maps.CameraOptions
+import com.mapbox.maps.MapView
 import com.dasumma.mobileskinav.ui.theme.MobileSkiNavTheme
+import com.mapbox.android.core.permissions.PermissionsListener
+import com.mapbox.android.core.permissions.PermissionsManager
 
-class MainActivity : ComponentActivity() {
+
+class MainActivity : ComponentActivity(), PermissionsListener {
+
+    private lateinit var permissionsManager: PermissionsManager
+    private lateinit var mapView: MapView
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        permissionsManager = PermissionsManager(this)
+        if (PermissionsManager.areLocationPermissionsGranted(baseContext)) {
+            createContent()
+        } else {
+            permissionsManager = PermissionsManager(this)
+            permissionsManager.requestLocationPermissions(this)
+        }
         enableEdgeToEdge()
-        setContent {
-            MobileSkiNavTheme {
-                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
-                    )
-                }
-            }
+    }
+
+    fun createContent(){
+        mapView = MapView(this)
+        mapView.mapboxMap.setCamera(
+            CameraOptions.Builder()
+                .center(Point.fromLngLat(-98.0, 39.5))
+                .pitch(0.0)
+                .zoom(2.0)
+                .bearing(0.0)
+                .build()
+        )
+        setContentView( mapView )
+    }
+
+    override fun onRequestPermissionsResult(
+        requestCode: Int,
+        permissions: Array<out String?>,
+        grantResults: IntArray,
+        deviceId: Int
+    ) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults, deviceId)
+    }
+
+    override fun onExplanationNeeded(permissionsToExplain: List<String>) {
+
+    }
+
+    override fun onPermissionResult(granted: Boolean) {
+
+        if (granted) {
+            createContent()
+        } else {
+
         }
     }
 }
+
 
 @Composable
 fun Greeting(name: String, modifier: Modifier = Modifier) {
