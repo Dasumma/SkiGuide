f9c418e1db0580b0ea68a647e3256bcbc6e4ab32
Dasumma <dasumma1@gmail.com>
2026-03-19 22:32:50 -0400

    Added SkiGuide API Project

---
+++ SkiGuideAPI/src/main/java/net/dasumma1/skiguideapi/SkiguideapiApplication.java
@@ -0,0 +1,13 @@
+package net.dasumma1.skiguideapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SkiguideapiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SkiguideapiApplication.class, args);
+	}
+
+}
---
+++ SkiGuideAPI/src/test/java/net/dasumma1/skiguideapi/SkiguideapiApplicationTests.java
@@ -0,0 +1,13 @@
+package net.dasumma1.skiguideapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SkiguideapiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
